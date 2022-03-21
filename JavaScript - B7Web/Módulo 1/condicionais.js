85125160b72c89778edc058be72d05049fcae24a
Aleff Lira <dev.alefflira@gmail.com>
2022-03-20 22:03:41 -0300

    Primeiro Commit

---
+++ JavaScript - B7Web/Módulo 1/condicionais.js
@@ -0,0 +1,11 @@
+// Variáveis
+let hora = 17;
+
+// Condicionais
+if (hora < 12) {
+    window.alert('Bom dia!');
+} else if (hora < 18) {
+    window.alert('Boa tarde!');
+} else if (hora < 23) {
+    window.alert('Boa noite!');
+}
